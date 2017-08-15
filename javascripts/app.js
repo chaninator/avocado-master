adaae2b73cfebecc87ce35196b94af930ac61c10
Andrew Chan <accsuperstar@gmail.com>
2017-08-15 10:03:46 -0500

    added lightbox

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,11 +1,20 @@
-window.onresize = function(){ location.reload(); }
+console.log('JS sanity check');
+
+window.onresize = function() {
+  location.reload();
+}
 
 $(function() {
 
   var window_size = window.matchMedia('(max-width: 450px)');
 
+  // media query for mobile device
   if (window.matchMedia('(max-width: 450px)').matches) {
-    $.ajax({url: 'javascripts/avocado.json', data: {}}).done(function(response) {
+    $.ajax(
+      {
+        url: 'javascripts/avocado.json',
+        data: {}
+      }).done(function(response) {
       console.log(response);
       var listMarkup = '';
       for (var i = 0; i < response.length; i++) {
@@ -18,13 +27,17 @@ $(function() {
       console.log("complete");
     });
   }
-
+  // media query for desktop
   else {
-    $.ajax({url: 'javascripts/avocado.json', data: {}}).done(function(response) {
-      console.log(response);
+    $.ajax(
+      {
+        url: 'javascripts/avocado.json',
+        data: {}
+      }).done(function(response) {
+      console.log('response:', response);
       var listMarkup = '';
       for (var i = 0; i < response.length; i++) {
-        listMarkup += '<div class="avocado-item"><h2 class="avocado-number">' + (i + 1) + '</h2><img src="../images/' + response[i].img + '" class="lightbox_trigger"><div><h3 class="avocado-title">' + response[i].name + '</h3>' + '<p>' + response[i].description + '</p></div></div>';
+        listMarkup += '<div class="avocado-item"><h2 class="avocado-number">' + (i + 1) + '</h2><img src="../images/' + response[i].img + '" class="avocado-img"><div><h3 class="avocado-title">' + response[i].name + '</h3>' + '<p>' + response[i].description + '</p></div></div>';
       }
       $('#avocado-list').append(listMarkup);
     }).fail(function() {
@@ -34,24 +47,30 @@ $(function() {
     });
   };
 
-  // lightbox not completed
+  // lightbox for avocado thumbnails
 
-  $('.lightbox_trigger').on('click', function(e) {
-    e.preventDefault();
-    console.log("clicked");
-    var image_src = $(this).attr('src');
-    if ($('#lightbox').length > 0) {
-      $('#content').html('<img src="' + image_src + '" />');
-      $('#lightbox').show();
-    } else {
-      var lightbox = '<div id="lightbox">' +
-      '<p>Click to close</p>' +
-      '<div id="content">' +
-      '<img src="' + image_src + '" />' + '</div></div>';
-      $('body').append(lightbox);
-    }
+  var overlay = $('<div id="lightbox"></div>');
+  var img = $('<img>');
+  var close = $('<span>&#10005;</span>');
+  var imgUrl;
+
+  $('body').append(overlay);
+  overlay.hide();
+
+  // event listener to open lightbox on image click
+  $(document).on('click', 'img.avocado-img', function() {
+    console.log('click success!');
+    imgUrl = $(this).attr('src'); // get img url
+    console.log('image url:', imgUrl);
+    overlay.append(img);
+    overlay.append(close);
+    img.attr('src', imgUrl);
+    overlay.fadeIn('1000');
+    img.fadeIn('1000');
   });
-  $('#lightbox').on('click', function() {
-    $('#lightbox').hide();
+
+  // close lightbox
+  close.click(function() {
+    overlay.fadeOut('1000');
   });
 });
